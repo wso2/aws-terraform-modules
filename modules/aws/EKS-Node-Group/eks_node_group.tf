6e7593a9cd408dd7e05887ee60397ec4805ea43f
Ranika <ranikamadurawe@gmail.com>
2023-02-15 16:59:41 +0530

    Add instance type for EKS

--- modules/aws/EKS-Node-Group/eks_node_group.tf
+++ modules/aws/EKS-Node-Group/eks_node_group.tf
@@ -15,6 +15,7 @@ resource "aws_eks_node_group" "eks_node_group" {
   node_role_arn   = aws_iam_role.iam_role.arn
   subnet_ids      = var.subnet_ids
   version         = var.k8s_version
+  instance_types  = var.instance_types
 
   dynamic "taint" {
     for_each = var.taints
