5b5f88af3f5578e4adf5ca364d1605af0dc0cf28
Ranika <ranikamadurawe@gmail.com>
2023-04-12 16:56:39 +0530

    Add transit gateway modules

---
+++ modules/aws/Transit-Gateway-VPC-Attatchment/transit_gateway.tf
@@ -0,0 +1,18 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "ec2_transit_gateway_vpc_attachment" {
+  for_each = var.vpc_map
+
+  transit_gateway_id = var.transit_gateway_id
+  vpc_id             = each.value.vpc_id
+  subnet_ids         = each.value.subnet_ids
+}
---
+++ modules/aws/WAF-Web-ACL/waf_web_acl.tf
@@ -0,0 +1,126 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+resource "aws_wafv2_web_acl" "web_acl" {
+  name               = var.name
+  scope              = var.scope
+  description        = var.description
+  
+  visibility_config {
+    cloudwatch_metrics_enabled = var.cloudwatch_metrics_enabled
+    metric_name                 = var.cloudwatch_metric_name
+    sampled_requests_enabled   = var.sampled_requests_enabled
+  }
+
+  default_action {
+    dynamic "allow" {
+      for_each = var.default_action.type == "allow" ? [1] : []
+      content {
+        dynamic "custom_request_handling" {
+          for_each = var.default_action.insert_header == null ? [1] : []
+          content {
+            insert_header {
+              name  = var.default_action.insert_header.name
+              value = var.default_action.insert_header.value
+            }
+          }
+        }
+      }
+    }
+    dynamic "block" {
+      for_each = var.default_action.type == "block" ? [1] : []
+      content {
+        dynamic "custom_response" {
+          for_each = var.default_action.response_header == null ? [1] : []
+          content {
+            custom_response_body_key = var.default_action.custom_response_body_key
+            response_code = var.default_action.response_code
+            response_header {
+              name  = var.default_action.response_header.name
+              value = var.default_action.response_header.value
+            }
+          }
+        }
+      }
+    }
+  }
+
+  dynamic "custom_response_body" {
+    for_each = var.custom_response_body != null ? var.custom_response_body : {}
+
+    content {
+      content      = var.custom_response_body.content
+      content_type = var.custom_response_body.content_type
+      key   = var.custom_response_body.key
+    }
+  }
+
+  dynamic "rule" {
+    for_each = var.rules
+
+    content {
+      name     = rule.value.name
+      priority = rule.value.priority
+      action {
+        dynamic "allow" {
+          for_each = rule.value.action.type == "allow" ? [1] : []
+          content {
+            dynamic "custom_request_handling" {
+              for_each = rule.value.action.insert_header == null ? [1] : []
+              content {
+                insert_header {
+                  name  = rule.value.action.insert_header.name
+                  value = rule.value.action.insert_header.value
+                }
+              }
+            }
+          }
+        }
+        dynamic "block" {
+          for_each = rule.value.action.type == "block" ? [1] : []
+          content {
+            dynamic "custom_response" {
+              for_each = rule.value.action.response_header == null ? [1] : []
+              content {
+                custom_response_body_key = rule.value.action.custom_response_body_key
+                response_code = rule.value.action.response_code
+                response_header {
+                  name  = rule.value.action.response_header.name
+                  value = rule.value.action.response_header.value
+                }
+              }
+            }
+          }
+        }
+      }
+
+      override_action {
+        dynamic "count" {
+          for_each = rule.value.override_action.type == "count" ? [1] : []
+          content {}
+        }
+        dynamic "null" {
+          for_each = rule.value.override_action.type == "null" ? [1] : []
+          content {}
+        }
+      }
+
+      statement {
+
+      }
+      visibility_config {
+        cloudwatch_metrics_enabled = rule.value.cloudwatch_metrics_enabled
+        metric_name                 = rule.value.cloudwatch_metric_name
+        sampled_requests_enabled   = rule.value.sampled_requests_enabled
+      }
+    }
+  }
+}
