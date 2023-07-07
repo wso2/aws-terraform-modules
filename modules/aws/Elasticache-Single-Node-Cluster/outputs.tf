920f5b61f01c8b2a2d6f84071cc819e1d04ea3f8
Ranika <ranikamadurawe@gmail.com>
2023-07-07 10:50:21 +0530

    Add outputs to TF

---
+++ modules/aws/Elasticache-Single-Node-Cluster/outputs.tf
@@ -0,0 +1,23 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+output "primary_endpoint_address" {
+  value      = aws_elasticache_replication_group.single_node_elasticache_replication_group.primary_endpoint_address
+  depends_on = [aws_elasticache_replication_group.single_node_elasticache_replication_group]
+}
+output "redis_cluster_id" {
+  value      = aws_elasticache_replication_group.single_node_elasticache_replication_group.id
+  depends_on = [aws_elasticache_replication_group.single_node_elasticache_replication_group]
+}
+ouptut "node_id" {
+  value      = join("-", [aws_elasticache_replication_group.single_node_elasticache_replication_group.id, "001"])
+  depends_on = [aws_elasticache_replication_group.single_node_elasticache_replication_group]
+}
