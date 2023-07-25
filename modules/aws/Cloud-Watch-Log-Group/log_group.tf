9ac2397d8aac41ee61a1ea3827d75c0bdd0af049
Ranika <ranikamadurawe@gmail.com>
2023-07-25 10:22:36 +0530

    Add Log Group module

---
+++ modules/aws/Cloud-Watch-Log-Group/log_group.tf
@@ -0,0 +1,16 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+resource "aws_cloudwatch_log_group" "log_group" {
+  name = var.log_group_name
+  retention_in_days = var.retention_in_days
+  tags = var.default_tags
+}
---
+++ modules/aws/Cloud-Watch-Log-Group/variables.tf
@@ -0,0 +1,25 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+variable "log_group_name" {
+  description = "The name of the log group to create."
+  type        = string
+}
+variable "default_tags" {
+  description = "Default tags for resources."
+  type        = map(string)
+  default     = {}
+}
+variable "retention_in_days" {
+  description = "Number of days to retain log events."
+  type        = number
+  default     = 30
+}
