f7bc3d7dcb2475e44cf4b6b134ffccdd59959918
Ranika <ranikamadurawe@gmail.com>
2023-08-15 12:33:43 +0530

    Add modules for logging

---
+++ modules/aws/CloudTrail-Logs/variables.tf
@@ -0,0 +1,107 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+variable "enable_log_file_validation" {
+  type        = bool
+  default     = true
+  description = "Specifies whether log file integrity validation is enabled. Creates signed digest for validated contents of logs"
+}
+variable "is_multi_region_trail" {
+  type        = bool
+  default     = true
+  description = "Specifies whether the trail is created in the current region or in all regions"
+}
+variable "include_global_service_events" {
+  type        = bool
+  default     = false
+  description = "Specifies whether the trail is publishing events from global services such as IAM to the log files"
+}
+variable "enable_logging" {
+  type        = bool
+  default     = true
+  description = "Enable logging for the trail"
+}
+variable "s3_bucket_name" {
+  type        = string
+  description = "S3 bucket name for CloudTrail logs"
+}
+variable "cloud_watch_logs_role_arn" {
+  type        = string
+  description = "Specifies the role for the CloudWatch Logs endpoint to assume to write to a user’s log group"
+  default     = ""
+}
+variable "cloud_watch_logs_group_arn" {
+  type        = string
+  description = "Specifies a log group name using an Amazon Resource Name (ARN), that represents the log group to which CloudTrail logs will be delivered"
+  default     = ""
+}
+variable "insight_selector" {
+  type = list(object({
+    insight_type = string
+  }))
+
+  description = "Specifies an insight selector for type of insights to log on a trail"
+  default     = []
+}
+variable "event_selector" {
+  type = list(object({
+    include_management_events = bool
+    read_write_type           = string
+
+    data_resource = list(object({
+      type   = string
+      values = list(string)
+    }))
+  }))
+
+  description = "Specifies an event selector for enabling data event logging. See: https://www.terraform.io/docs/providers/aws/r/cloudtrail.html for details on this variable"
+  default     = []
+}
+variable "kms_key_arn" {
+  type        = string
+  description = "Specifies the KMS key ARN to use to encrypt the logs delivered by CloudTrail"
+  default     = ""
+}
+variable "is_organization_trail" {
+  type        = bool
+  default     = false
+  description = "The trail is an AWS Organizations trail"
+}
+variable "sns_topic_name" {
+  type        = string
+  description = "Specifies the name of the Amazon SNS topic defined for notification of log file delivery"
+  default     = null
+}
+variable "s3_key_prefix" {
+  type        = string
+  description = "Prefix for S3 bucket used by Cloudtrail to store logs"
+  default     = null
+}
+variable "project" {
+  type        = string
+  description = "Name of the project"
+}
+variable "environment" {
+  type        = string
+  description = "Name of the environment"
+}
+variable "region" {
+  type        = string
+  description = "Code of the region"
+}
+variable "application" {
+  type        = string
+  description = "Purpose of the Subnet"
+}
+variable "tags" {
+  type        = map(string)
+  description = "Tags to be added to the resources created"
+}
---
+++ modules/aws/VPC-Flow-Log/iam.tf
@@ -0,0 +1,52 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_role" {
+  count = var.log_destination_type == "cloudwatch" ? 1 : 0
+  name               = join("-", [var.project, var.application, var.environment, var.region, "flog-log-iam-role"])
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "iam_policy_document" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "iam_role_policy" {
+  count = var.log_destination_type == "cloudwatch" ? 1 : 0
+  name   = join("-", [var.project, var.application, var.environment, var.region, "flog-log-iam-role-policy"])
+  role   = aws_iam_role.iam_role.0.id
+  policy = data.aws_iam_policy_document.iam_policy_document.json
+}
