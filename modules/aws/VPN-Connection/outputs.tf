ceca4af06c96b267bece729ee29f2d4208ca4b95
Ranika <ranikamadurawe@gmail.com>
2023-09-07 12:22:50 +0530

    Add gateway attachment id as output

--- modules/aws/VPN-Connection/outputs.tf
+++ modules/aws/VPN-Connection/outputs.tf
@@ -25,3 +25,7 @@ output "vpn_connection_tunnel1_address" {
   value      = aws_vpn_connection.vpn_connection.tunnel1_address
   depends_on = [aws_vpn_connection.vpn_connection]
 }
+output "vpn_connection_transit_gateway_connection_id" {
+  value = aws_vpn_connection.vpn_connection.transit_gateway_attachment_id
+  depends_on = [aws_vpn_connection.vpn_connection]
+}
