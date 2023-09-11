0c57d378bfd069605d74c815d4e455b55a887264
Ranika <ranikamadurawe@gmail.com>
2023-09-11 12:15:48 +0530

    Update Access key rule creation

--- modules/aws/IAM_User_Access_Key/iam_user_access_key.tf
+++ modules/aws/IAM_User_Access_Key/iam_user_access_key.tf
@@ -8,23 +8,11 @@
 # You may not alter or remove any copyright or other notice from copies of this content.
 #
 # --------------------------------------------------------------------------------------
-resource "time_rotating" "time_rotating" {
-  rotation_days = var.rotation_days
-}
-
-# which then updates the leapfrog toggle
-resource "toggles_leapfrog" "toggle" {
-  trigger = time_rotating.time_rotating.rotation_rfc3339
-}
 
 resource "aws_iam_access_key" "iam_access_key" {
   user = var.iam_user_name
 
   lifecycle {
-    replace_triggered_by = [
-      toggles_leapfrog.toggle.alpha,
-    ]
-
     # And we always want some credentials to exist, so create before destroy
     create_before_destroy = true
   }
