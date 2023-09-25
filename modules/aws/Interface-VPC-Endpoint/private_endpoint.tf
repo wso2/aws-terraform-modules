93cbc1413da0e6c4f146a18b15f80aeaa2efbc3c
Ranika <ranikamadurawe@gmail.com>
2023-09-25 11:05:57 +0530

    Fix variable issue

--- modules/aws/Interface-VPC-Endpoint/private_endpoint.tf
+++ modules/aws/Interface-VPC-Endpoint/private_endpoint.tf
@@ -10,7 +10,7 @@
 # --------------------------------------------------------------------------------------
 
 resource "aws_vpc_endpoint" "vpc_endpoint" {
-  vpc_id            = var
+  vpc_id            = var.vpc_id
   service_name      = var.service_name
   vpc_endpoint_type = "Interface"
 
