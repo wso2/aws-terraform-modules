d90b6b392abe6eeba5ed05a53f7bd6ca84a4092d
Ranika <ranikamadurawe@gmail.com>
2023-10-12 11:26:51 +0530

    Add ECR repo name output

--- modules/aws/ECR/outputs.tf
+++ modules/aws/ECR/outputs.tf
@@ -25,3 +25,7 @@ output "ecr_arn" {
   value      = aws_ecr_repository.ecr_repository.arn
   depends_on = [aws_ecr_repository.ecr_repository]
 }
+output "ecr_repository_url" {
+  value      = aws_ecr_repository.ecr_repository.repository_url
+  depends_on = [aws_ecr_repository.ecr_repository]
+}
