ac7002b1a56aaf3c153500e68b9d47b65d28e8ad
Ranika <ranikamadurawe@gmail.com>
2023-11-15 09:44:25 +0530

    Add outputs for EC2 instance

--- modules/aws/EC2-Instance/outputs.tf
+++ modules/aws/EC2-Instance/outputs.tf
@@ -13,7 +13,27 @@ output "ec2-instance-arn" {
   value      = aws_instance.ec2_instance.arn
   depends_on = [aws_instance.ec2_instance]
 }
+output "ec2-instance-id" {
+    value      = aws_instance.ec2_instance.id
+    depends_on = [aws_instance.ec2_instance]
+}
 output "ec2-instance-role-name" {
   value      = aws_iam_role.iam_role.name
   depends_on = [aws_iam_role.iam_role]
+}
+output "ec2-instance-subnet-id" {
+  value      = aws_subnet.ec2_subnet.id
+  depends_on = [aws_subnet.ec2_subnet]
+}
+output "ec2-instance-subnet-arn" {
+  value      = aws_subnet.ec2_subnet.arn
+  depends_on = [aws_subnet.ec2_subnet]
+}
+output "ec2-instance-route-table-id" {
+  value      = aws_route_table.route_table.id
+  depends_on = [aws_route_table.route_table]
+}
+output "ec2-instance-route-table-arn" {
+  value      = aws_route_table.route_table.arn
+  depends_on = [aws_route_table.route_table]
 }
