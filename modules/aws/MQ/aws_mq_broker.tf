62fe3c84fffa1a5214632d133021d5232da442c7
hiranadikari993 <hiranadikari993@gmail.com>
2024-01-22 14:26:40 +0530

    Add AmazonMQ module

---
+++ modules/aws/MQ/aws_mq_broker.tf
@@ -0,0 +1,36 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+resource "aws_mq_broker" "mq" {
+  broker_name = join("-", [var.project, var.application, var.environment, var.region, "mq"])
+
+  engine_type = "ActiveMQ"
+  engine_version = var.engine_version
+  host_instance_type = var.instance_type
+  security_groups = var.security_group_ids
+  subnet_ids = var.subnet_ids
+  publicly_accessible = var.public_access
+  auto_minor_version_upgrade = var.auto_minor_version_upgrade
+
+  dynamic "user" {
+    for_each = var.users
+    content {
+      username = user.value.username
+      password = user.value.password
+      console_access = user.value.console_access
+    }
+  }
+
+  logs {
+    audit = var.audit_logs_enabled
+    general = var.general_logs_enabled
+  }
+}
