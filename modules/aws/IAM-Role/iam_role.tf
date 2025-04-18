cad3db68e32b0c2c9ecbe74a3ae44e87fd8a52bc
osura <osuraw@wso2.com>
2025-04-18 09:46:54 +0530

    Update IAM User module to support multiple policy attachments; remove IAM user policy attachment module.

--- modules/aws/IAM-Role/iam_role.tf
+++ modules/aws/IAM-Role/iam_role.tf
@@ -10,13 +10,13 @@
 # --------------------------------------------------------------------------------------
 
 resource "aws_iam_role" "iam_role" {
-  name                = join("-", [var.project, var.application, var.environment, var.region, "iam-role"])
-  tags                = var.tags
-  assume_role_policy  = var.assume_role_policy
+  name               = join("-", [var.project, var.application, var.environment, var.region, "iam-role"])
+  tags               = var.tags
+  assume_role_policy = var.assume_role_policy
 }
 
 resource "aws_iam_role_policy_attachment" "policy-attachment" {
-  count = length(var.policy_arns)
+  count      = length(var.policy_arns)
   role       = aws_iam_role.iam_role.name
   policy_arn = var.policy_arns[count.index]
 }
