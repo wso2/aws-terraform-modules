1d0d72f0b004fe242ffcd8d0e73c35ac2dfb2ee9
sachithkasthuriarachchi <sachithka@wso2.com>
2025-04-25 09:01:25 +0530

    Add EKS module compatible with Cilium

---
+++ modules/aws/EKS-Cluster-No-CNI/add_ons.tf
@@ -0,0 +1,15 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+resource "aws_eks_addon" "coredns" {
+  cluster_name = aws_eks_cluster.eks_cluster.name
+  addon_name   = "coredns"
+}
