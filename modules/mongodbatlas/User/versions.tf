ca8fbb6bb428de2dd0a28b4868a475f796a36731
Shan Anjana <shananjanajayaweera@gmail.com>
2025-05-15 11:34:03 +0530

    Update versions to trigger the cla test

--- modules/mongodbatlas/User/versions.tf
+++ modules/mongodbatlas/User/versions.tf
@@ -6,7 +6,7 @@
 # Dissemination of any information or reproduction of any material contained
 # herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
 # You may not alter or remove any copyright or other notice from copies of this content.
-#
+# 
 # --------------------------------------------------------------------------------------
 
 terraform {
