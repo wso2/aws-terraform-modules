2b4f823c92657005d3d3397bce7acb1b6f3b926f
Ranika Madurawe <ranikamadurawe@gmail.com>
2025-05-15 13:17:11 +0530

    Add versions

--- modules/aws/RDS-MSSQL-IAM/iam.tf
+++ modules/aws/RDS-MSSQL-IAM/iam.tf
@@ -1,3 +1,14 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
 resource "aws_iam_instance_profile" "rds_instance_profile" {
   name = "AWSRDSCustomSQLServerInstanceProfile"
   role = aws_iam_role.rds_role.name
