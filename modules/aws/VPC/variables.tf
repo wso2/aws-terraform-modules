df50f8dfc77da1158793e569525d07b022a5c6ef
Ruwin Ratnayake <ruwin@wso2.com>
2026-01-16 14:44:44 +0530

    Update copyright year and license information

--- modules/aws/VPC/variables.tf
+++ modules/aws/VPC/variables.tf
@@ -1,30 +1,23 @@
 # -------------------------------------------------------------------------------------
 #
-# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+# Copyright (c) 2026, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
 #
-# This software is the property of WSO2 LLC. and its suppliers, if any.
-# Dissemination of any information or reproduction of any material contained
-# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
-# You may not alter or remove any copyright or other notice from copies of this content.
+# WSO2 LLC. licenses this file to you under the Apache License,
+# Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
 #
 # --------------------------------------------------------------------------------------
 
-# variable "project" {
-#   type        = string
-#   description = "Name of the project"
-# }
-# variable "environment" {
-#   type        = string
-#   description = "Name of the environment"
-# }
-# variable "region" {
-#   type        = string
-#   description = "Code of the region"
-# }
-# variable "application" {
-#   type        = string
-#   description = "Purpose of the Subnet"
-# }
 variable "vpc_cidr_block" {
   type        = string
   description = "CIDR block to be used for the VPC"
