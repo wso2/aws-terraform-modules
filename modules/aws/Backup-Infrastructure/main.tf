794ec583fca19c7fceb7a41c99ab1f912053662a
ruwin_rmrrr <ruwinrathnayake@gmail.com>
2026-01-19 15:17:04 +0530

    Address coderabbit comments

--- modules/aws/Backup-Infrastructure/main.tf
+++ modules/aws/Backup-Infrastructure/main.tf
@@ -171,7 +171,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "backup_reports" {
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm     = "aws:kms"
-      kms_master_key_id = aws_kms_key.backup.arn
+      kms_master_key_id = aws_kms_key.key.arn
     }
   }
 }
