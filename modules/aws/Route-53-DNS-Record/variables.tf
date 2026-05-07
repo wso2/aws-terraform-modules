36242be60c604d7d633315880bac957c7f65ab2c
udhanMti <udhanisuranga@gmail.com>
2026-05-07 15:09:58 +0530

    Add alias support to Route-53-DNS-Record module

--- modules/aws/Route-53-DNS-Record/variables.tf
+++ modules/aws/Route-53-DNS-Record/variables.tf
@@ -18,14 +18,31 @@ variable "name" {
   type        = string
 }
 variable "ttl" {
-  description = "Time to Live of the record"
+  description = "Time to Live of the record. Must be null when alias_dns_name is set."
   type        = number
+  default     = null
 }
 variable "type" {
-  description = "Type of the A Record"
+  description = "Type of the DNS record (e.g. A, CNAME, NS)"
   type        = string
 }
 variable "records" {
-  description = "Records to be associated with the DNS entry"
+  description = "Records to be associated with the DNS entry. Must be null when alias_dns_name is set."
   type        = list(string)
+  default     = null
+}
+variable "alias_dns_name" {
+  description = "The DNS name of the target for an alias record (e.g. an ELB hostname). When set, ttl and records must be null."
+  type        = string
+  default     = null
+}
+variable "alias_hosted_zone_id" {
+  description = "The hosted zone ID of the alias target (e.g. the ELB's AWS-owned hosted zone ID). Required when alias_dns_name is set."
+  type        = string
+  default     = null
+}
+variable "alias_evaluate_target_health" {
+  description = "Whether to evaluate the health of the alias target."
+  type        = bool
+  default     = true
 }
