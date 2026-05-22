60c45a002d54846b6d3f8d1d041945331183fd2e
udhanMti <udhanisuranga@gmail.com>
2026-05-22 13:16:07 +0530

    Add metric support for Autoscaling-Group module

--- modules/aws/Autoscaling-Group/autoscaling_group.tf
+++ modules/aws/Autoscaling-Group/autoscaling_group.tf
@@ -1,8 +1,8 @@
 # -------------------------------------------------------------------------------------
 #
-# Copyright (c) 2026, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+# Copyright (c) 2026, WSO2 LLC (http://www.wso2.com). All Rights Reserved.
 #
-# This software is the property of WSO2 LLC. and its suppliers, if any.
+# This software is the property of WSO2 LLC and its suppliers, if any.
 # Dissemination of any information or reproduction of any material contained
 # herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
 # You may not alter or remove any copyright or other notice from copies of this content.
@@ -19,6 +19,8 @@ resource "aws_autoscaling_group" "autoscaling_group" {
   health_check_type         = var.health_check_type
   health_check_grace_period = var.health_check_grace_period
   default_cooldown          = var.default_cooldown
+  enabled_metrics           = var.enabled_metrics
+  metrics_granularity       = length(var.enabled_metrics) > 0 ? "1Minute" : null
 
   launch_template {
     id      = var.launch_template_id
