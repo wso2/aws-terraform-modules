3b9b540370b9167eae48b69b5cef06103c4b71d9
Ravindran Dharshan <dharshanravindran8@gmail.com>
2026-05-28 14:04:24 +0530

    Update the public licenses  header and variable names in Custom AMI module

--- modules/aws/Custom-AMI/variables.tf
+++ modules/aws/Custom-AMI/variables.tf
@@ -1,44 +1,53 @@
 # -------------------------------------------------------------------------------------
 #
-# Copyright (c) 2026, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+# Copyright (c) 2026, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
 #
-# This software is the property of WSO2 LLC. and its suppliers, if any.
-# Dissemination of any information or reproduction of any material contained
-# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
-# You may not alter or remove any copyright or other notice from copies of this content.
+# WSO2 LLC. licenses this file to you under the Apache License,
+# Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
 #
 # --------------------------------------------------------------------------------------
 
-variable "custom_ami_name" {
+variable "ami_name" {
   description = "A region-unique name for the AMI."
   type        = string
 }
 
-variable "custom_ami_description" {
+variable "ami_description" {
   description = "A longer, human-readable description for the AMI."
   type        = string
   default     = null
 }
 
-variable "custom_ami_architecture" {
+variable "ami_architecture" {
   description = "Machine architecture for created instances. Defaults to arm64."
   type        = string
   default     = "arm64"
 }
 
-variable "custom_ami_virtualization_type" {
+variable "ami_virtualization_type" {
   description = "Keyword to choose what virtualization mode created instances will use."
   type        = string
   default     = "hvm"
 }
 
-variable "custom_ami_imds_support" {
+variable "ami_imds_support" {
   description = "If v2.0 is selected, EC2 instances will only support IMDSv2."
   type        = string
   default     = "v2.0"
 }
 
-variable "custom_ami_tags" {
+variable "ami_tags" {
   description = "A map of tags to assign to the resource."
   type        = map(string)
 }
